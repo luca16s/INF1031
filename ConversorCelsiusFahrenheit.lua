9c32ec7e02a2c0343ae68b36e634a5ee2cdaaec3
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-08-31 15:42:41 -0300

    - Incluir a formatação da string

--- ConversorCelsiusFahrenheit.lua
+++ ConversorCelsiusFahrenheit.lua
@@ -1,4 +1,4 @@
 print("Digite uma temperatura em Celsius: ")
 celsius = io.read("*n")
 fahrenheit = 1.8 * celsius + 32
-print("O valor em Fahrenheit: ", fahrenheit)
+print(string.format("O valor em Fahrenheit: %s", fahrenheit))
