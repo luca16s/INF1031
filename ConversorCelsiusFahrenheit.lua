753821c4f2042e6f3db5038efba1b208e573c373
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-08-31 15:53:06 -0300

    - Definir variáveis como locais

--- ConversorCelsiusFahrenheit.lua
+++ ConversorCelsiusFahrenheit.lua
@@ -1,4 +1,4 @@
 print("Digite uma temperatura em Celsius: ")
-celsius = io.read("*n")
-fahrenheit = 1.8 * celsius + 32
+local celsius = io.read("*n")
+local fahrenheit = 1.8 * celsius + 32
 print(string.format("O valor em Fahrenheit: %s", fahrenheit))
