324effa53141f2c66c5c77932ec7d00c2c961a6f
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-08-31 16:26:54 -0300

    - Adicionar a função de conversão inversa de Fahrenheit para celsius

--- ConversorCelsiusFahrenheit.lua
+++ ConversorCelsiusFahrenheit.lua
@@ -1,7 +1,20 @@
-function ConvertParaFahrenheit(temperatura)
+function ConverteParaFahrenheit(temperatura)
     return 1.8 * temperatura + 32;
+end
+
+function ConverteParaCelsius(temperatura)
+    return (temperatura - 32) / 1.8 ;
+end
+
+function EstaComFebre(temperatura)
+ return temperatura >= 98.6;
 end
 
 print("Digite uma temperatura em Celsius: ")
 local celsius = io.read("*n")
-print(string.format("O valor em Fahrenheit: %s", ConvertParaFahrenheit(celsius)))
+print(string.format("O valor em Fahrenheit: %s", ConverteParaFahrenheit(celsius)))
+print(string.format("Está com febre: %s", EstaComFebre(ConverteParaFahrenheit(celsius))))
+print("Digite uma temperatura em Fahrenheit: ")
+local fahrenheit = io.read("*n")
+print(string.format("O valor em Celsius: %s", ConverteParaCelsius(fahrenheit)))
+print(string.format("Está com febre: %s", EstaComFebre(ConverteParaCelsius(fahrenheit))))
