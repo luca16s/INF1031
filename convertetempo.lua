e598b26026f818e6aab19bf7f11a2df8c3506f9d
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-09-02 18:00:49 -0300

    - Aplicar melhorias de código

--- convertetempo.lua
+++ convertetempo.lua
@@ -1,6 +1,6 @@
 -- Resolução exercício 1
 print("Insira a quantidade de segundos: ")
-local tempoInformado = 117100--io.read("*n")
+local tempoInformado = io.read("*n")
 
 local quantidadeSegundosDia = 24*60*60
 local quantidadeSegundosHora = 60*60
@@ -20,10 +20,21 @@ print(segundos)
 
 print(string.format("%s dias, %s horas, %s minutos e %s segundos", dias, horas, minutos, segundos))
 
+
 -- Resolução exercício 3
-local mensagem
+
+function InsereVirgula(mensagem)
+  if mensagem ~= nil and string.len(mensagem) > 1 then
+    return mensagem .. ", "
+  else
+    return mensagem
+  end
+end
+
+local mensagem = ""
 
 if dias > 0 then
+  mensagem = InsereVirgula(mensagem)
   if dias == 1 then
     mensagem = string.format("%s dia", dias)
   else
@@ -32,26 +43,29 @@ if dias > 0 then
 end
 
 if horas > 0 then
+  mensagem = InsereVirgula(mensagem)
   if horas == 1 then
-    mensagem = mensagem .. string.format(", %s hora", horas)
+    mensagem = mensagem .. string.format("%s hora", horas)
   else
-    mensagem = mensagem .. string.format(", %s horas", horas)
+    mensagem = mensagem .. string.format("%s horas", horas)
   end
 end
 
 if minutos > 0 then
+  mensagem = InsereVirgula(mensagem)
   if minutos == 1 then
-    mensagem = mensagem .. string.format(", %s minuto", minutos)
+    mensagem = mensagem .. string.format("%s minuto", minutos)
   else
-    mensagem = mensagem .. string.format(", %s minutos", minutos)
+    mensagem = mensagem .. string.format("%s minutos", minutos)
   end
 end
 
 if segundos > 0 then
+  mensagem = InsereVirgula(mensagem)
   if segundos == 1 then
-    mensagem = mensagem .. string.format(", %s segundo", segundos)
+    mensagem = mensagem .. string.format("%s segundo", segundos)
   else
-    mensagem = mensagem .. string.format(", %s segundos", segundos)
+    mensagem = mensagem .. string.format("%s segundos", segundos)
   end
 end
 
