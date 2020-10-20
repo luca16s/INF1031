2c297982ee0f6d4cb209f211b42e7e862ce972fe
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-10-20 20:57:00 -0300

    LAB7

--- LAB7/easytext.lua
+++ LAB7/easytext.lua
@@ -10,16 +10,25 @@ local cores = {
 
   local ancoras = {
     -- valores válidos para ancoras:
-    no = true, n = true, ne = true, o = true, c = true, e = true, so = true, s = true, se = true}
+    no = true,
+    n = true,
+    ne = true,
+    o = true,
+    c = true,
+    l = true,
+    so = true,
+    s = true,
+    se = true
+  }
 
   local text = {}
 
   text.new = function (fontname, tam, str)
     local meutexto = {}
+    tam = tam or 32    
     local fonte = love.graphics.newFont(fontname .. ".ttf", tam)
     local textoLove = love.graphics.newText(fonte, str)
     meutexto.textoLove = textoLove
-    meutexto.cor = cores.black
     return meutexto
   end
 
@@ -28,45 +37,85 @@ local cores = {
   end
 
   text.setColor = function (meutexto, cor1, cor2, cor3)
-    meutexto.cor = cores.black
-    
+    if cor1 == nil then
+      return cores.black
+    end
     if type(cor1) == "string" then
-      -- verifica se passou uma cor válida
-      if cores[cor1]  ~= nil then
-        meutexto.cor = cores[cor1]
+      if cores[cor1] == nil then
+        return cores.black
       end
+      return cores[cor1]
     else
-      -- assumo que passou diretamente uma tripla rgb
-      meutexto.cor = {cor1, cor2, cor3}
+      return {cor1, cor2, cor3}
+    end
+  end  
+
+  text.setFillColor = function (meutexto, cor1, cor2, cor3)
+    if type(cor1) == "string" then
+      love.graphics.setColor(text.setColor(meutexto, cor1))
+    else
+      love.graphics.setColor(text.setColor(meutexto, cor1, cor2, cor3))
     end
   end
 
-  text.setFillColor = function (meutexto, cor1, cor2, cor3)
-    -- implementar
-  end
-
   text.setLineColor = function (meutexto, cor1, cor2, cor3)
-    -- implementar
+    if cor1 == nil then
+      assert (cores[cor1], "cor inexistente")
+    end
+    if type(cor1) == "string" then
+      if cores[cor1] == nil then
+        return cores.black
+      end
+      love.graphics.setColor(cores[cor1])
+    else
+      love.graphics.setColor(cor1, cor2, cor3)
+    end    
   end
 
   local function cantoesqsup (x, y, dx, dy, ancora)
-    if ancora == "c" then 
-      x = x - dx/2
+    if ancora == "no" then
+      x = x
+      y = y
+    elseif ancora == "o" then
+      x = x
       y = y - dy/2
+    elseif ancora == "so" then
+      x = x
+      y = y + dy
+    elseif ancora == "n" then
+      x = x - dx/2
+    elseif ancora == "l" then
+      x = x - dx
+      y = y - dy/2
+    elseif ancora == "s" then
+      x = x - dx/2
+      y = y + dy
     elseif ancora == "ne" then
       x = x - dx
-    else
-      -- outros casos
+    elseif ancora == "c" then 
+      x = x - dx/2
+      y = y - dy/2
+    elseif ancora == "se" then
+      x = x - dx
+      y = y + dy
     end
     return x, y
   end
 
-  text.draw = function (meutexto, x, y, ancora)
+  text.draw = function (meutexto, x, y, ancora, corFonte, corRetangulo)
     local dx, dy = meutexto.textoLove:getDimensions()
-    love.graphics.setColor(meutexto.cor)
+    text.setFillColor(meutexto, corFonte)
+    
     ancora = ancora or "c"
     assert (ancoras[ancora], "ancora inexistente")
-    local novox, novoy = cantoesqsup (x, y, dx, dy, ancora)
+    
+    local novox, novoy = cantoesqsup(x, y, dx, dy, ancora)
+    
+    if corRetangulo ~= nil then
+      love.graphics.rectangle("line", novox, novoy, text.getDimensions(meutexto))
+      text.setLineColor(meutexto, corRetangulo)
+    end
+    
     love.graphics.draw(meutexto.textoLove, novox, novoy)
   end
 
--- LAB7/main.lua
+++ LAB7/main.lua
@@ -1,37 +1,82 @@
 local easytext = require "easytext"
 local maxpals = 10
 local textos = {}
+local id, ancora, corFonte, corRetangulo = nil
 
 function love.load ()
-
   textos[1] = easytext.new("game", 32, "palavra 1")
-  easytext.setColor (textos[1], "red")
-
-  textos[2] = easytext.new("horrendo", 32, "palavra 2")
-  easytext.setColor (textos[2], "light_blue")
-
+  textos[2] = easytext.new("horrendo", nil, "palavra 2")
   textos[3] = easytext.new("stocky", 32, "palavra 3")
-  easytext.setColor (textos[3], "green")
-
+  textos[4] = easytext.new("game", 32, "palavra 4")
+  textos[5] = easytext.new("horrendo", 32, "palavra 5")
+  textos[6] = easytext.new("stocky", 32, "palavra 6")
+  textos[7] = easytext.new("game", 32, "palavra 7")
+  textos[8] = easytext.new("horrendo", 32, "palavra 8")
+  textos[9] = easytext.new("stocky", 32, "palavra 9")
 end
 
 function mostrapals ()
+  if id == nil or ancora == nil then
+    return
+  end
+  
   local w, h = love.graphics.getDimensions()
   local x = w/2
-  local y = 30
-  for i = 1, #textos do
-    easytext.draw(textos[i], x, y)
-    local tx, ty = easytext.getDimensions(textos[i])
-    y = y + ty
-  end
-  for i = 1, #textos do
-    easytext.draw(textos[i], x, y, "ne")
-    local tx, ty = easytext.getDimensions(textos[i])
-    y = y + ty
-  end
+  local y = h/2
+  
+  easytext.draw(textos[id], x, y, ancora, corFonte, corRetangulo)
+  local tx, ty = easytext.getDimensions(textos[id])
+  y = y + ty
 end
 
 function love.draw()
-  love.graphics.setBackgroundColor(0.2,0.2,0.2)
+  love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
   mostrapals()
+end
+
+function love.keypressed (key)  
+  if key == "kp1" then
+    id = 1
+    ancora = "no"
+    corFonte = "black"
+    corRetangulo = "white"
+  elseif key == "kp2" then
+    id = 2
+    ancora = "o"
+    corFonte = "blue"
+    corRetangulo = nil
+  elseif key == "kp3" then
+    id = 3
+    ancora = "so"
+    corFonte = nil
+    corRetangulo = "light_green"
+  elseif key == "kp4" then
+    id = 4
+    ancora = "n"
+    corFonte = nil
+    corRetangulo = nil
+  elseif key == "kp5" then
+    id = 5
+    ancora = "l"
+    corFonte = "orange"
+    corRetangulo = "purple"
+  elseif key == "kp6" then
+    id = 6
+    ancora = "s"
+    corFonte = nil
+    corRetangulo = nil
+  elseif key == "kp7" then
+    id = 7
+    ancora = "ne"
+  elseif key == "kp8" then
+    id = 8
+    ancora = "c"
+    corFonte = nil
+    corRetangulo = nil
+  elseif key == "kp9" then
+    id = 9
+    ancora = "se"
+    corFonte = nil
+    corRetangulo = nil
+  end
 end
