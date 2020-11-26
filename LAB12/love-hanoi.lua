bcaf62b6437a48470394b04c3f4e90b0d71252f1
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-11-26 20:19:24 -0300

    - 2

--- LAB12/love-hanoi.lua
+++ LAB12/love-hanoi.lua
@@ -1,27 +1,29 @@
 local torres = { {}, {}, {} }
-local numdiscos = 5
+local numdiscos = 3
 
 function desenha()
 -- melhorar o desenho dos discos!
   local larg, alt = love.graphics.getDimensions()
   local x, y
   love.graphics.setLineWidth(3)
+  local fonte = love.graphics.newFont("Game.ttf", 32)
+  local contagemCirculos = 1
   for i = 1, 3 do
     x = i*larg/5 + larg/10
     y = alt
     for j = 1, #torres[i] do
-      love.graphics.setColor(1,1,1)
-      love.graphics.rectangle ("line", x - torres[i][j]/2, y - alt/15, torres[i][j], alt/15)
+      love.graphics.setColor(1, 1, 1)
+      love.graphics.rectangle("line", x - torres[i][j]/2, y - alt/15, torres[i][j], alt/15)
+      love.graphics.draw(love.graphics.newText(fonte, contagemCirculos), x - torres[i][j]/2, y - alt/15)
       y = y - alt/15
+      contagemCirculos = contagemCirculos + 1
     end
   end
 end
 
 local function move(origem, destino)
-  for i = numdiscos, 1, -1 do
-    table.insert(torres[destino], torres[origem][i])
-    table.remove(torres[origem], i)
-  end
+    table.insert(torres[destino], 1, torres[origem][1])
+    table.remove(torres[origem], 1)
 end
 
 local function hanoi(origem, destino, auxiliar, quantos)
@@ -29,7 +31,7 @@ local function hanoi(origem, destino, auxiliar, quantos)
     print (string.format("mova de %d para %d", origem, destino))
     move(origem, destino)
     -- inicialmente deixe comentado (questão 3) e depois descomente!
-    --sleep(1)
+    sleep(1)
   else -- completar
     -- usar 3 chamadas recursivas!!!
     hanoi(origem, auxiliar, destino, quantos-1)
